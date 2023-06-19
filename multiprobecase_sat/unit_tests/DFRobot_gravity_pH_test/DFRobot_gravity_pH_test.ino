003b945ff0afb3b37a8d657d39372b6039df5ea9
DeGaMon <quentin.gchnk@gmail.com>
2023-06-19 10:54:16 +0200

    Updated DFRobot Gravity pH sensor unit test

--- multiprobecase_sat/unit_tests/DFRobot_gravity_pH_test/DFRobot_gravity_pH_test.ino
+++ multiprobecase_sat/unit_tests/DFRobot_gravity_pH_test/DFRobot_gravity_pH_test.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+#define PH_PIN  33
 
+#include "DFRobot_PH.h"
+#include <EEPROM.h>
+
+float voltage,phValue,temperature = 25;
+DFRobot_PH ph;
+
+void setup()
+{
+    Serial.begin(115200);  
+    ph.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    static unsigned long timepoint = millis();
+    if(millis()-timepoint>1000U){                  //time interval: 1s
+        timepoint = millis();
+        //temperature = readTemperature();         // read your temperature sensor to execute temperature compensation
+        voltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
+        phValue = ph.readPH(voltage,temperature);  // convert voltage to pH with temperature compensation
+        Serial.print("temperature:");
+        Serial.print(temperature,1);
+        Serial.print("^C  pH:");
+        Serial.println(phValue,2);
+    }
+    ph.calibration(voltage,temperature);           // calibration process by Serail CMD
+}
 
+float readTemperature()
+{
+  //add your code here to get the temperature from your temperature sensor
 }
